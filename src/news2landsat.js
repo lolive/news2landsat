09525744835fc103947c94761784910ba949ca17
daniel.latapie <daniel.latapie@akka.eu>
2013-08-02 15:26:57 +0200

    Extraction of annotation into annotate.js file

--- src/news2landsat.js
+++ src/news2landsat.js
@@ -16,44 +16,11 @@ var conf = require('./conf.js');
 var httpReq = require('./httpReq.js');
 var boilerPipe = require('./boilerPipe.js');
 var clavin = require('./clavin.js');
+var annotate = require('./annotate.js');
 
 exports.resolveLocations=clavin.resolveLocations;
-
-function annotateText(geonames, articleText) {
-    var qq = Q.defer();
-    var annotateLocations = function (whatToDoWithAnnotatedText, whatToDoOnAnnotationError) {
-        try {
-            var annotatedText = articleText;
-            if (geonames) {
-                var locations = JSON.parse(geonames);
-                if (locations.resolvedLocations) {
-                    locations.resolvedLocations.forEach(function (resolvedLocation) {
-                        var name = resolvedLocation.inputName;
-                        var long = resolvedLocation.geoname.longitude;
-                        var lat = resolvedLocation.geoname.latitude;
-                        annotatedText = annotateLocation(annotatedText, name, long, lat);
-                    });
-                }
-            }
-            return whatToDoWithAnnotatedText(annotatedText);
-        } catch (e) {
-            whatToDoOnAnnotationError(e);
-        }
-    };
-    annotateLocations(qq.resolve, qq.reject);
-    return qq.promise;
-}
-exports.annotateText = annotateText;
-
-function annotateLocation(text, name, long, lat) {
-
-    var replacementText = "$1<a href=\"http://localhost/sparql2mapWidget/GoogleEarth/index.xhtml?long=" + long + "&lat=" + lat + "&name=" +encodeURIComponent(name)+ "\">_" + "$2" + "_</a>$3";
-    var regexp = new RegExp("([^_]*)" + "("+name+")" + "([^_]*)", "gi");
-    if(text)
-        return text.replace(regexp, replacementText);
-
-}
-exports.annotateLocation = annotateLocation;
+exports.annotateText = annotate.annotateText;
+exports.annotateLocation = annotate.annotateLocation;
 
 http.createServer(function (proxyReq, proxyResp) {
 
@@ -81,7 +48,7 @@ http.createServer(function (proxyReq, proxyResp) {
 //
 //    var articleToHtml = toArticle(articleSrc);
 
-    boilerPipe.articleContent(articleSrc).spread(clavin.resolveLocations).spread(annotateText).then(sendResultBackToBrowser).fail(sendErrorBackToBrowser);
+    boilerPipe.articleContent(articleSrc).spread(clavin.resolveLocations).spread(annotate.annotateText).then(sendResultBackToBrowser).fail(sendErrorBackToBrowser);
 
 
 //    var destParams = url.parse(URL);
