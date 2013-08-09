ce815909d751b8fc8605232754ded8a0ee5d2714
daniel.latapie <daniel.latapie@akka.eu>
2013-08-09 14:33:21 +0200

    Set and unset environment variables in conf file

--- src/conf.js
+++ src/conf.js
@@ -1,13 +1,14 @@
 exports.port = process.env.port || 8185;
-exports.proxyOptions = {
-    host: "192.168.254.1",
-    port: 9090,
-    headers: {
-        'Proxy-Authorization': 'Basic ' + new Buffer("OLIVIER.ROSSEL" + ':' + "7512OLRO*").toString('base64')
-    }
-}
 exports.theProxy = "http://OLIVIER.ROSSEL:7512OLRO*@proxy2.akka.eu:9090";
 exports.proxyEnabled = true;
 
 exports.boilerpipeInputUrl = "http://boilerpipe-web.appspot.com/extract?extractor=ArticleExtractor&output=text&extractImages=&url=";
 exports.clavinUrl = "http://clavin.berico.us/clavin-web/Services/GeoExtract/ResolvedLocations";
+
+exports.urlStoreArticle = "http://sitools.akka.eu:8183/sitools/datastorage/user/semantic-storage/fr/";
+exports.urlUpdateWebSemanticTree = "http://sitools.akka.eu:8183/sitools/datastorage/user/semantic-storage/json/data_fr.json";
+
+exports.logSitools = {
+        id: "admin",
+        pass: "sitoolsisthebest"
+}
